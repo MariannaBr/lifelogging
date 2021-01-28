610577cf210b92a720bd599ec3399ad28c95c108
marianna <marianna@primeflow.com>
2021-01-28 16:23:54 +0100

    set up firebase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b434e809b14a918226f9";
+/******/ 	var hotCurrentHash = "5086806a1c69de35be2a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
 import Header from "../components/Header"
-import Category from "../components/Category"
-import inputs from "../public/inputs"
-import Statistics from "../components/Statistics"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD61dFVqFCDh029N8Rr_PQjhJg5xAFOcRk",
+    authDomain: "lifelogging-20c8a.firebaseapp.com",
+    projectId: "lifelogging-20c8a",
+    storageBucket: "lifelogging-20c8a.appspot.com",
+    messagingSenderId: "111948075126",
+    appId: "1:111948075126:web:318b732adea514f32a0f57",
+    measurementId: "G-TRMZ4QPD92"
+  }
+  
+  firebase.initializeApp(firebaseConfig);
 
 function HomePage() {
 
